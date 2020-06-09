1fce83ca6f438bcb5dd31023367018666ceb4630
Chuck <chuck-bates@pluralsight.com>
2020-06-09 10:15:36 -0600

    Update scanner to look in cache first, then call api

--- src/scanner.py
+++ src/scanner.py
@@ -1,14 +1,18 @@
-barcodeDefs = {}
+import inventory_repository as repo
+import nutritionix_api_wrapper as api_wrapper
 
-def load():
-    with open('../barcodes.txt', encoding='utf8') as f:
-        barcodes = f.read().splitlines()
-        for barcode in barcodes:
-            parts = barcode.split(',')
-            barcodeDefs[parts[0]] = parts[1]
-    return
-
-def lookup(barcode):
-    if barcode in barcodeDefs:
-        return barcodeDefs.get(barcode)
-    return 'Unknown Barcode'
+def scan(barcode):
+    scan_response = {}
+    item = repo.find(barcode)
+    if item == None:
+        api_response = api_wrapper.find(barcode)
+        if api_response['successful'] == True:
+            scan_response['status'] = 'partial'
+            scan_response['item'] = api_response['item']
+        else:            
+            scan_response['status'] = 'not found'
+            scan_response['item'] = {'_id': barcode}
+    else:
+        scan_response['status'] = 'successful'
+        scan_response['item'] = item
+    return scan_response
--- test/scanner_test.py
+++ test/scanner_test.py
@@ -2,17 +2,33 @@ import sys
 sys.path.insert(1, '../src')
 
 import scanner
+import inventory_repository as repo
 
-class TestLoadBarcodeDefinitions:
-    def test_load(self):
-        assert len(scanner.barcodeDefs) == 0
-        scanner.load()
-        assert len(scanner.barcodeDefs) > 0
+_id = "123456"
 
-class TestLookupBarcode:
-    def test_lookup_unknown_barcode(self):
-        unknown = '000000'
-        assert scanner.lookup(unknown) == 'Unknown Barcode'
-    def test_lookup_known_barcode(self):
-        known = '720103002748'
-        assert scanner.lookup(known) == 'Ashbys flavorful assortment of teas 1.92oz'
+repo.load()
+
+inventory_item = {
+    '_id': _id,
+    'name': 'item_name',
+    'quantity': 1,
+    'size': 8,
+    'uom': 'oz',
+    'image': 'https://cdn.com/item.jpg'
+}
+
+repo.save(inventory_item)
+
+class Test_when_scanning_a_barcode_and_it_is_in_cache:
+    def test_it_should_return_the_inventory_item(self):
+        result = scanner.scan(_id)
+
+        assert result['name'] == inventory_item['name']
+        
+        repo.delete(_id)
+
+class Test_when_scanning_a_barcode_and_it_is_not_in_cache:
+    def test_it_should_return_the_nutritionix_result(self):
+        result = scanner.scan('851045005013')
+
+        assert result['name'] == 'Three Jerks Beef Jerky, Chipotle Adobo'
