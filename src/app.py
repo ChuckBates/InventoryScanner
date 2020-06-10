eb5b2826f515fde51fd8ffe07adad7c1747b79dd
Chuck <chuck-bates@pluralsight.com>
2020-06-10 15:41:08 -0600

    Handle basic scan in/out by increment/decrement

--- src/app.py
+++ src/app.py
@@ -1,14 +1,36 @@
 import inventory_lookup
+import inventory_repository as repo
 from guizero import App, Text, TextBox, PushButton, Box, Picture
 from tkinter import Label, PhotoImage
 
-def update_item_display():
-    found_item = inventory_lookup.find(barcode.value)
-    if found_item['status'] == 'successful':
-        item_found(found_item['item'])
+def update_item_display(mode):
+    if mode == 'look_up':
+        handle_item_look_up(inventory_lookup.find(look_up_barcode.value))    
+    elif mode == 'scan_in':
+        handle_item_scan_in(inventory_lookup.find(scan_in_barcode.value))
+    elif mode == 'scan_out':
+        handle_item_scan_out(inventory_lookup.find(scan_out_barcode.value))
+
+def handle_item_look_up(item):
+    if item['status'] == 'successful':
+        item_found(item['item'])
     else:
         item_name.value = 'Unknown item'
-    barcode.value = ''
+    look_up_barcode.value = ''
+
+def handle_item_scan_in(item):
+    if item['status'] == 'successful':
+        item['item']['quantity'] = item['item']['quantity'] + 1
+        repo.save(item['item'])
+        item_found(item['item'])
+    scan_in_barcode.value = ''
+
+def handle_item_scan_out(item):
+    if item['status'] == 'successful':
+        item['item']['quantity'] = item['item']['quantity'] - 1
+        repo.save(item['item'])
+        item_found(item['item'])
+    scan_out_barcode.value = ''
 
 def item_found(item_info):
     item_name.value = item_info['name']
@@ -27,22 +49,47 @@ def clear_info():
     item_size.value = ''
     reset_image()
 
-def key_pressed(event_data):
+def scan_in_key_pressed(event_data):
     reset_timer_to_reset_display()
     if 'Scan barcode to begin' in event_data.widget.value:
         event_data.widget.clear()
     if event_data.key == '\r':
         clear_info()
-        update_item_display()
+        update_item_display('scan_in')
+
+def scan_out_key_pressed(event_data):
+    reset_timer_to_reset_display()
+    if 'Scan barcode to begin' in event_data.widget.value:
+        event_data.widget.clear()
+    if event_data.key == '\r':
+        clear_info()
+        update_item_display('scan_out')
+
+def look_up_key_pressed(event_data):
+    reset_timer_to_reset_display()
+    if 'Scan barcode to begin' in event_data.widget.value:
+        event_data.widget.clear()
+    if event_data.key == '\r':
+        clear_info()
+        update_item_display('look_up')
 
 def scan_in_item():
-    set_display_to_scan()
+    scan_in_barcode.show()
+    scan_out_barcode.hide()
+    look_up_barcode.hide()
+    set_display_to_scan_in()
 
 def scan_out_item():
-    set_display_to_scan()
+    scan_in_barcode.hide()
+    scan_out_barcode.show()
+    look_up_barcode.hide()
+    set_display_to_scan_out()
 
 def look_up_item():
-    set_display_to_scan()
+    scan_in_barcode.hide()
+    scan_out_barcode.hide()
+    look_up_barcode.show()
+    set_display_to_look_up()
 
 def reset_display():
     item_info_box.hide()
@@ -59,7 +106,25 @@ def get_picture_width():
     space_width = int(int(app.tk.winfo_width() / 4) / 8)
     return app.tk.winfo_width() - (space_width * 2)
 
-def set_display_to_scan():    
+def set_display_to_scan_in(): 
+    set_display_to_blank()   
+    scan_in_barcode.focus()
+    scan_in_barcode.append('Scan barcode to begin')
+    app.update()
+
+def set_display_to_scan_out(): 
+    set_display_to_blank()   
+    scan_out_barcode.focus()
+    scan_out_barcode.append('Scan barcode to begin')
+    app.update()
+
+def set_display_to_look_up(): 
+    set_display_to_blank()   
+    look_up_barcode.focus()
+    look_up_barcode.append('Scan barcode to begin')
+    app.update()
+
+def set_display_to_blank():
     left_space_text.width = get_spacer_width()
     right_space_text.width = get_spacer_width()
     clear_info()
@@ -68,11 +133,8 @@ def set_display_to_scan():
     right_space_box.show()
     item_picture_box.show()
     buttons_box.hide()
-    barcode.focus()
-    barcode.append('Scan barcode to begin')
     reset_timer_to_reset_display()
     reset_image()
-    app.update()
 
 def reset_timer_to_reset_display():
     buttons_box.cancel(reset_display)
@@ -110,11 +172,24 @@ right_space_box = Box(app, height='fill', align='right')
 right_space_text = Text(right_space_box, height='fill', width='fill', text='')
 
 item_info_box = Box(app, width='fill', layout='grid')
-barcode = TextBox(item_info_box, width=22, grid=[1,2,2,1], align='left')
-barcode.text_color = text_color
-barcode.font = text_font
-barcode.text_size = text_size
-barcode.when_key_pressed = key_pressed
+
+scan_in_barcode = TextBox(item_info_box, width=22, grid=[1,2,2,1], align='left')
+scan_in_barcode.text_color = text_color
+scan_in_barcode.font = text_font
+scan_in_barcode.text_size = text_size
+scan_in_barcode.when_key_pressed = scan_in_key_pressed
+
+scan_out_barcode = TextBox(item_info_box, width=22, grid=[1,2,2,1], align='left')
+scan_out_barcode.text_color = text_color
+scan_out_barcode.font = text_font
+scan_out_barcode.text_size = text_size
+scan_out_barcode.when_key_pressed = scan_out_key_pressed
+
+look_up_barcode = TextBox(item_info_box, width=22, grid=[1,2,2,1], align='left')
+look_up_barcode.text_color = text_color
+look_up_barcode.font = text_font
+look_up_barcode.text_size = text_size
+look_up_barcode.when_key_pressed = look_up_key_pressed
 
 item_name_label = Text(item_info_box, text='Name:', color=alt_text_color, font=text_font, size=text_size, grid=[1,3], align='left')
 item_quantity_label = Text(item_info_box, text='Quantity:', color=alt_text_color, font=text_font, size=text_size, grid=[1,4], align='left')
@@ -134,5 +209,5 @@ item_picture.config(image = picture)
 reset_display()
 
 app.bg = '#011627'
-app.set_full_screen()
+# app.set_full_screen()
 app.display()
