efce55894ff1a934916287e1d4256e4aa941b079
Chuck <chuck-bates@pluralsight.com>
2020-06-12 22:46:35 -0600

    set background color of picture_widget to same as app to hide residual rendering

--- src/widgets/picture_widget.py
+++ src/widgets/picture_widget.py
@@ -7,6 +7,6 @@ def get_picture(parent, image):
     picture = PhotoImage(file=image)
     item_picture = Label(parent)
     item_picture.image = picture
-    item_picture.config(image = picture)
+    item_picture.config(image = picture, background='#011627')
     item_picture.pack()
     return item_picture
