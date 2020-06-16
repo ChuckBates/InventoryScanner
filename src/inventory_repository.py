47381e7f032826a11a7708c9dda345ff28fb6132
Chuck <chuck-bates@pluralsight.com>
2020-06-16 11:48:12 -0600

    Change backend DB from mongodb to postgres (because mongo on a raspberry pi is dumb)

--- src/inventory_repository.py
+++ src/inventory_repository.py
@@ -1,11 +1,14 @@
-import pymongo
+import psycopg2
 
 mycol = ''
 def load():
-    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-    mydb = myclient['inventory_database']
-    global mycol
-    mycol = mydb['inventory']
+    global pg_connection
+    pg_connection = psycopg2.connect(host='postgres.local', database='inventory_scanner', user='postgres', password='postgres')
+    global cursor
+    cursor = pg_connection.cursor()
+
+def unload():
+    cursor.close()
 
 def save(inventory_item):    
     if '_id' not in inventory_item:
@@ -18,25 +21,41 @@ def save(inventory_item):
         raise ValueError('Missing required field: size')
     if 'uom' not in inventory_item:
         raise ValueError('Missing required field: uom')
-    
-    query = {'_id': inventory_item['_id']}
-    mydict = { 
-        '$set': {
-            'name': inventory_item['name'],
-            'quantity': inventory_item['quantity'],
-            'size': inventory_item['size'],
-            'uom': inventory_item['uom'],
-            'image': inventory_item['image']
-        }
-    }
-    mycol.update_one(query, mydict, upsert=True)
+
+    if find(inventory_item['_id']) is not None:
+        update(inventory_item)
+    else:
+        insert(inventory_item)
+
+def update(inventory_item):
+    sql = f"UPDATE inventory SET name = %s, quantity = %s, size = %s, uom = %s, image = %s WHERE barcode = %s"
+    cursor.execute(sql, (inventory_item['name'], inventory_item['quantity'], inventory_item['size'], inventory_item['uom'], inventory_item['image'], inventory_item['_id']))
+    pg_connection.commit()
+
+def insert(inventory_item):
+    sql = "INSERT INTO inventory (barcode, name, quantity, size, uom, image) VALUES ('"+inventory_item["_id"]+"', '"+inventory_item["name"]+"', '"+str(inventory_item["quantity"])+"', '"+str(inventory_item["size"])+"', '"+inventory_item["uom"]+"', '"+inventory_item["image"]+"')"
+    cursor.execute(sql)
+    pg_connection.commit()
 
 def find(_id):
-    document_count = mycol.count_documents({'_id': _id})
-    if document_count == 0:
+    sql = "SELECT * FROM inventory WHERE barcode = %s"
+    cursor.execute(sql, (_id,))
+    pg_connection.commit()
+
+    result = cursor.fetchone()
+    if result is None:
         return None
-    documents = mycol.find({'_id': _id})
-    return documents[0]
+    else:
+        return {
+            '_id': result[0],
+            'name': result[1],
+            'quantity': result[2],
+            'size': result[3],
+            'uom': result[4],
+            'image': result[5]
+        }
 
 def delete(_id):
-    mycol.delete_one({"_id": _id})
+    sql = f"DELETE FROM inventory WHERE barcode = %s"
+    cursor.execute(sql, (_id,))
+    pg_connection.commit()
