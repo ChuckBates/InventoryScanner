cfc404cf837805eba6d108626adfa0ce479e4cc1
Chuck <chuck-bates@pluralsight.com>
2020-06-15 21:12:55 -0600

    refactor key_pad_widget to make shift key smoother

--- src/widgets/key_pad_widget.py
+++ src/widgets/key_pad_widget.py
@@ -10,20 +10,17 @@ def click(btn):
         paint_number_pad()
     elif btn == 'abc':
         destroy_pad()
-        paint_lower_letter_pad()
+        paint_letter_pad()
     elif btn == '⌫':
         remove_last_letter()
     elif btn == '↑':
-        print(is_upper_case())
         global case
         if is_upper_case():
             case = 'lower'
-            destroy_pad()
-            paint_lower_letter_pad()
+            shift_letters('lower')
         else:
             case = 'upper'
-            destroy_pad()
-            paint_upper_letter_pad()
+            shift_letters('upper')
     else:
         text_container.append(btn)
 
@@ -66,10 +63,18 @@ def spin_up_keypad(tc):
 
     global case
     case = 'lower'
-    paint_lower_letter_pad()
+    paint_letter_pad()
     return label_frame
 
-def paint_lower_letter_pad():
+def shift_letters(direction):
+    for child in label_frame.winfo_children():
+        current = child['text']
+        if direction == 'upper':
+            child.config(text=current.upper())
+        else: 
+            child.config(text=current.lower())
+
+def paint_letter_pad():
     row_one_column=0
     row_two_column=0
     row_three_column=0
@@ -88,36 +93,6 @@ def paint_lower_letter_pad():
             btn.grid(row=2, column=row_two_column, columnspan=2)
             row_two_column+=1
         elif label in characters.letter_lower_third_row:
-            btn.grid(row=3, column=row_three_column, columnspan=2)
-            row_three_column+=1
-        elif label in characters.letter_fourth_row:
-            if label == ' ':
-                btn = tkinter.Button(label_frame, text=label, width=32, command=cmd, bg="#011627", fg=display_config.text_color, font=display_config.text_font, relief='groove')
-                btn.grid(row=4, column=row_four_column, columnspan=6)
-                row_four_column+=4
-            else:
-                btn.grid(row=4, column=row_four_column, columnspan=3)
-                row_four_column+=1
-
-def paint_upper_letter_pad():
-    row_one_column=0
-    row_two_column=0
-    row_three_column=0
-    row_four_column=0
-
-    btn = list(range(len(characters.letter_upper_btn_list)+3))
-    for label in characters.letter_upper_btn_list:
-        cmd = partial(click, label)
-
-        key_width=6
-        btn = tkinter.Button(label_frame, text=label, width=key_width, command=cmd, bg="#011627", fg=display_config.text_color, font=display_config.text_font, relief='groove')
-        if label in characters.letter_upper_first_row:
-            btn.grid(row=1, column=row_one_column)
-            row_one_column+=1
-        elif label in characters.letter_upper_second_row:        
-            btn.grid(row=2, column=row_two_column, columnspan=2)
-            row_two_column+=1
-        elif label in characters.letter_upper_third_row:
             btn.grid(row=3, column=row_three_column, columnspan=2)
             row_three_column+=1
         elif label in characters.letter_fourth_row:
