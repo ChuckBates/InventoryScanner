46a36d6ad039e271f10732f87c53569a2b104d28
Chuck <chuck-bates@pluralsight.com>
2020-06-17 22:56:31 -0600

    Hook server into postgres

--- src/server/inventorysite/inventorysite/settings.py
+++ src/server/inventorysite/inventorysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'inventorysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inventory_scanner',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres.local',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MDT'
 
 USE_I18N = True
 
