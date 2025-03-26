e7dde3ce83b8b8ff22d52975efb38080dee65e75
Chuck <chuck.bates@transport4.com>
2025-03-26 11:53:05 -0600

    Remove Swagger

--- InventoryScannerCore/Program.cs
+++ InventoryScannerCore/Program.cs
@@ -18,13 +18,6 @@ builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
